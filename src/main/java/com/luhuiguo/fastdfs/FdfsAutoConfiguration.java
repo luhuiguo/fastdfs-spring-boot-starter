e0dc84ab57cde5e6eadf2fd2e5cb1987e639d8b0
luhuiguo <luhuiguo@gmail.com>
2017-03-08 10:16:37 +0800

    no message

---
+++ src/main/java/com/luhuiguo/fastdfs/FdfsAutoConfiguration.java
@@ -0,0 +1,72 @@
+package com.luhuiguo.fastdfs;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.luhuiguo.fastdfs.conn.ConnectionManager;
+import com.luhuiguo.fastdfs.conn.ConnectionPoolConfig;
+import com.luhuiguo.fastdfs.conn.FdfsConnectionPool;
+import com.luhuiguo.fastdfs.conn.PooledConnectionFactory;
+import com.luhuiguo.fastdfs.conn.TrackerConnectionManager;
+import com.luhuiguo.fastdfs.service.DefaultFastFileStorageClient;
+import com.luhuiguo.fastdfs.service.DefaultTrackerClient;
+import com.luhuiguo.fastdfs.service.FastFileStorageClient;
+import com.luhuiguo.fastdfs.service.TrackerClient;
+
+@Configuration
+@EnableConfigurationProperties(FdfsProperties.class)
+public class FdfsAutoConfiguration {
+
+  private final FdfsProperties properties;
+
+  public FdfsAutoConfiguration(FdfsProperties properties) {
+    super();
+    this.properties = properties;
+  }
+
+  @Bean
+  public PooledConnectionFactory pooledConnectionFactory() {
+    PooledConnectionFactory pooledConnectionFactory = new PooledConnectionFactory();
+    pooledConnectionFactory.setSoTimeout(properties.getSoTimeout());
+    pooledConnectionFactory.setConnectTimeout(properties.getConnectTimeout());
+    return pooledConnectionFactory;
+  }
+
+
+  @Bean
+  public ConnectionPoolConfig connectionPoolConfig() {
+    ConnectionPoolConfig connectionPoolConfig = new ConnectionPoolConfig();
+    //connectionPoolConfig.setJmxEnabled(false);
+    return connectionPoolConfig;
+  }
+
+  @Bean
+  public FdfsConnectionPool fdfsConnectionPool(PooledConnectionFactory pooledConnectionFactory,
+      ConnectionPoolConfig connectionPoolConfig) {
+    FdfsConnectionPool pool =  new FdfsConnectionPool(pooledConnectionFactory, connectionPoolConfig);
+    return pool;
+  }
+
+  @Bean
+  public TrackerConnectionManager trackerConnectionManager(FdfsConnectionPool fdfsConnectionPool) {
+    return new TrackerConnectionManager(fdfsConnectionPool, properties.getTrackerList());
+  }
+
+  @Bean
+  public TrackerClient trackerClient(TrackerConnectionManager trackerConnectionManager) {
+    return new DefaultTrackerClient(trackerConnectionManager);
+  }
+
+  @Bean
+  public ConnectionManager connectionManager(FdfsConnectionPool fdfsConnectionPool) {
+    return new ConnectionManager(fdfsConnectionPool);
+  }
+
+  @Bean
+  public FastFileStorageClient storageClient(TrackerClient trackerClient,
+      ConnectionManager connectionManager) {
+    return new DefaultFastFileStorageClient(trackerClient, connectionManager);
+  }
+
+}
---
+++ src/main/java/com/luhuiguo/fastdfs/FdfsProperties.java
@@ -0,0 +1,45 @@
+package com.luhuiguo.fastdfs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "fdfs")
+public class FdfsProperties {
+
+  /** 读取时间 */
+  private int soTimeout = 1000;
+  /** 连接超时时间 */
+  private int connectTimeout = 1000;
+
+  /** Tracker 地址列表 */
+  private List<String> trackerList = new ArrayList<>();
+
+  public int getSoTimeout() {
+    return soTimeout;
+  }
+
+  public void setSoTimeout(int soTimeout) {
+    this.soTimeout = soTimeout;
+  }
+
+  public int getConnectTimeout() {
+    return connectTimeout;
+  }
+
+  public void setConnectTimeout(int connectTimeout) {
+    this.connectTimeout = connectTimeout;
+  }
+
+  public List<String> getTrackerList() {
+    return trackerList;
+  }
+
+  public void setTrackerList(List<String> trackerList) {
+    this.trackerList = trackerList;
+  }
+
+  
+  
+}
