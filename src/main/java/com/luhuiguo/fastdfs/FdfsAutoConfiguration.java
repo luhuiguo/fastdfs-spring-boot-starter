ab0496de565b12a5cb486f65a516a961c1fbb5e7
luhuiguo <luhuiguo@gmail.com>
2017-06-05 07:50:12 +0800

    no message

--- src/main/java/com/luhuiguo/fastdfs/FdfsAutoConfiguration.java
+++ src/main/java/com/luhuiguo/fastdfs/FdfsAutoConfiguration.java
@@ -1,5 +1,6 @@
 package com.luhuiguo.fastdfs;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -37,9 +38,9 @@ public class FdfsAutoConfiguration {
 
 
   @Bean
+  @ConfigurationProperties(prefix = "fdfs.pool")
   public ConnectionPoolConfig connectionPoolConfig() {
     ConnectionPoolConfig connectionPoolConfig = new ConnectionPoolConfig();
-    //connectionPoolConfig.setJmxEnabled(false);
     return connectionPoolConfig;
   }
 
